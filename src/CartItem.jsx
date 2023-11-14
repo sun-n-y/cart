4594afbc16294298923d5be8992d14e068715488
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-14 11:20:12 -0500

    add reudcer to global context

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,9 +1,5 @@
 import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
-import { useGlobalContext } from './context';
 const CartItem = ({ id, img, title, price, amount }) => {
-  const { greeting } = useGlobalContext();
-  console.log(greeting);
-
   return (
     <article className="cart-item">
       <img src={img} alt={title} />
