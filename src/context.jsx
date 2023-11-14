780a9d7de0f70dd5bf00335f31bb5a8127bab280
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-14 12:50:39 -0500

    add remove functionality

--- src/context.jsx
+++ src/context.jsx
@@ -24,8 +24,12 @@ export const AppProvider = ({ children }) => {
     dispatch({ type: CLEAR_CART });
   };
 
+  const remove = (id) => {
+    dispatch({ type: REMOVE, payload: { id } });
+  };
+
   return (
-    <AppContext.Provider value={{ ...state, clearCart }}>
+    <AppContext.Provider value={{ ...state, clearCart, remove }}>
       {children}
     </AppContext.Provider>
   );
--- src/reducer.js
+++ src/reducer.js
@@ -12,5 +12,11 @@ export const reducer = (state, action) => {
     return { ...state, cart: new Map() };
   }
 
+  if (action.type === REMOVE) {
+    const newCart = new Map(state.cart);
+    newCart.delete(action.payload.id);
+    return { ...state, cart: newCart };
+  }
+
   throw new Error(`no matching action type : ${action.type}`);
 };
