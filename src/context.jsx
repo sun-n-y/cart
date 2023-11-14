55c365df17527d38c55fbe3ea267e6fb777107b6
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-14 13:21:00 -0500

    add total cost functionality

--- src/context.jsx
+++ src/context.jsx
@@ -9,6 +9,7 @@ import {
   DISPLAY_ITEMS,
 } from './actions';
 import cartItems from './data';
+import { getTotals } from './utils';
 
 const AppContext = createContext();
 
@@ -19,6 +20,8 @@ const initialState = {
 
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { totalAmount, totalCost } = getTotals(state.cart);
 
   const clearCart = () => {
     dispatch({ type: CLEAR_CART });
@@ -38,7 +41,15 @@ export const AppProvider = ({ children }) => {
 
   return (
     <AppContext.Provider
-      value={{ ...state, clearCart, remove, increase, decrease }}
+      value={{
+        ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+        totalAmount,
+        totalCost,
+      }}
     >
       {children}
     </AppContext.Provider>
