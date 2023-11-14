e839f4f94fcf13f37f28b9eede34be8c8a9600d0
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-14 13:27:42 -0500

    add fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 // components
 import Navbar from './Navbar';
 import CartContainer from './CartContainer';
+import { useGlobalContext } from './context';
 
 function App() {
+  const { loading } = useGlobalContext();
+
+  if (loading) {
+    return (
+      <main>
+        <div className="loading"></div>
+      </main>
+    );
+  }
+
   return (
     <main>
       <Navbar />
--- src/context.jsx
+++ src/context.jsx
@@ -11,10 +11,12 @@ import {
 import cartItems from './data';
 import { getTotals } from './utils';
 
+const url = 'https://course-api.com/react-useReducer-cart-project';
+
 const AppContext = createContext();
 
 const initialState = {
-  loading: false,
+  loading: true,
   cart: new Map(cartItems.map((item) => [item.id, item])),
 };
 
@@ -38,6 +40,16 @@ export const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({ type: DECREASE, payload: { id } });
   };
+
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const cart = await response.json();
+    console.log(cart);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <AppContext.Provider
